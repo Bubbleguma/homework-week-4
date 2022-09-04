25f599fba8ce07e6d1b52bfdaa72e37795da34d6
Bubbleguma <angelkovacs18@gmail.com>
2022-09-04 15:31:49 +0200

    updatery

--- src/weather.js
+++ src/weather.js
@@ -29,36 +29,41 @@ function chang(event) {
   lin.innerHTML = `21`;
 }
 
-function showTemperature(response) {
+function displayWeatherCondition(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#num").innerHTML = Math.round(
     response.data.main.temp
   );
 }
 
-function showPosition(position) {
-  let apiKey = "89c9ecbba6dfb51142563de590c487ba";
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-  axios.get(`${apiUrl}`).then(showTemperature);
-}
-function getPosition(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(showPosition);
-}
 function searchCity(city) {
   let apiKey = "89c9ecbba6dfb51142563de590c487ba";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showTemperature);
+  axios.get(apiUrl).then(displayWeatherCondition);
 }
+
 function handleSubmit(event) {
   event.preventDefault();
-  let city = document.querySelector("#city-input").value;
+  let city = document.querySelector("#search-input").value;
   searchCity(city);
 }
+
+function searchLocation(position) {
+  let apiKey = "89c9ecbba6dfb51142563de590c487ba";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
 let searchForm = document.querySelector("#city");
 searchForm.addEventListener("submit", handleSubmit);
 
-let button = document.querySelector(".cur");
-button.addEventListener = ("click", getPosition);
+let currentButton = document.querySelector("#cur");
+currentButton.addEventListener("click", getCurrentLocation);
+
+searchCity("");
