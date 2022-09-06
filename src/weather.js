859e73df8998467d1c1f8963480755adfcc7edf5
Bubbleguma <angelkovacs18@gmail.com>
2022-09-06 10:32:20 +0200

    js

--- src/weather.js
+++ src/weather.js
@@ -14,47 +14,6 @@ let hour = now.getHours();
 let minute = now.getMinutes();
 date.innerHTML = `${day} ${hour}:${minute}`;
 
-function localValues(c) {
-  let temperature = Math.round(c.data.main.temp);
-  let changeTemp = document.querySelector("#temperatureToday");
-  changeTemp.innerHTML = temperature;
-
-  let city = c.data.name;
-  let localCity = document.querySelector("#city");
-  localCity.innerHTML = city;
-
-  let sky = c.data.weather[0].description;
-  sky = sky.charAt(0).toUpperCase() + sky.slice(1);
-  let localSky = document.querySelector("#sky");
-  localSky.innerHTML = sky;
-
-  let humidity = c.data.main.humidity;
-  let localHumidity = document.querySelector("#hum");
-  localHumidity.innerHTML = humidity;
-
-  let wind = Math.round(c.data.wind.speed);
-  let localWind = document.querySelector("#wind");
-  localWind.innerHTML = wind;
-}
-
-function emoji(c) {
-  let sky = c.data.weather[0].main;
-  let skyNow = document.querySelector(".emoji");
-  if (sky === "Clear") {
-    skyNow.innerHTML = "☀";
-  } else if (sky === "Clouds") {
-    skyNow.innerHTML = "🌤";
-  } else if (sky === "Drizzle") {
-    skyNow.innerHTML = "🌨";
-  } else if (sky === "Rain") {
-    skyNow.innerHTML = "🌧";
-  } else if (sky === "Thunderstorm") {
-    skyNow.innerHTML = "⚡️";
-  } else if (sky === "Snow") {
-    skyNow.innerHTML = "❄️";
-  }
-}
-
 function showCity(event) {
   event.preventDefault();
 
