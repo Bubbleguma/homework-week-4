257ea73e2e88db9d2a16a1f9e25186ca29f6ebd4
Bubbleguma <angelkovacs18@gmail.com>
2022-09-30 12:57:07 +0200

    fuing sheeeps

--- src/app.js
+++ src/app.js
@@ -68,7 +68,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "89c9ecbba6dfb51142563de590c487ba";
+  let apiKey = "a43564c91a6c605aeb564c9ed02e3858";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
